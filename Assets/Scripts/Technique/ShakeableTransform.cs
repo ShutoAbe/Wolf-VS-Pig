3902dd8e2d858a1061f69d540ab61fbcf4f66b43
k0k1i2i2 <kouki200012@gmail.com>
2022-09-07 10:46:58 +0900

    10:40 カメラ　振動追加

--- Assets/Scripts/Technique/ShakeableTransform.cs
+++ Assets/Scripts/Technique/ShakeableTransform.cs
@@ -3,18 +3,11 @@ using UnityEngine;
 
 public class ShakeableTransform : MonoBehaviour
 {
-    public ShakeableTransform m_shakeable;
-
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            m_shakeable.InduceStress(1);
+            gameObject.GetComponent<Cinemachine.CinemachineImpulseSource>().GenerateImpulse();
         }
-    }
-
-    private void InduceStress(int v)
-    {
-        throw new NotImplementedException();
-    }
+    }    
 }
